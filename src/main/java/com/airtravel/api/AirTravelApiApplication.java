e06a77497b33d2bea25e8fd53d61353c55512657
Valeriia <valeriiaholotiuk@gmail.com>
2025-10-27 17:37:45 -0230

    First commit: unzip

---
+++ src/main/java/com/airtravel/api/AirTravelApiApplication.java
@@ -0,0 +1,13 @@
+package com.airtravel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirTravelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirTravelApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airtravel/api/AirTravelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airtravel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTravelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
